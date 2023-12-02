0112e2a1962ae01a35f594ef7834342ef97cbd45
MG-elghandori <mohamedelghandori4@gmail.com>
2023-12-02 12:50:31 +0100

    small ft_printf

--- ft_printf.h
+++ ft_printf.h
@@ -1,4 +1,9 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-int     ft_printf(const char *str, ...);
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+int                 ft_printf(const char *str, ...);
+size_t				ft_strlen(const char *theString);
 #endif
