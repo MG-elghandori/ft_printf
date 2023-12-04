64d83b23935ca0e9ac175b4456ba9c6761ee94d9
MG-elghandori <mohamedelghandori4@gmail.com>
2023-12-04 07:35:39 +0100

    ajouter function ft_hexadecimal et creer p et u

--- ft_printf.h
+++ ft_printf.h
@@ -4,10 +4,12 @@
 #include <string.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <limits.h>
 
 int                 ft_printf(const char *str, ...);
 size_t				ft_strlen(const char *theString);
 int 				ft_putchar(char c);
-int 				ft_putnbr(int nb);
+int 				ft_putnbr(long long n);
 int					ft_putstr(char *c);
+int					ft_hexadecimal(unsigned long h);
 #endif
--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,19 +1,15 @@
 #include "ft_printf.h"
 
-int ft_putnbr(int nb)
+int ft_putnbr(long long n)
 {
-	int count;
-
-	count = 0;
-	unsigned int n;
-	if(nb < 0){
-		count += ft_putchar('-');
-		n = -nb;
-	}else {
-		n = nb;
-	}
-	if (n <= 9)
-		return count + ft_putchar(n + '0');
-	count += ft_putnbr(n / 10);
-    return count + ft_putchar(n % 10 + '0');
+    int count = 0;
+    if (n < 0)
+    {
+        count += ft_putchar('-');
+        n = -n;
+    }
+    if (n > 9)
+        count += ft_putnbr(n / 10);
+    count += ft_putchar(n % 10 + '0');
+    return count;
 }
